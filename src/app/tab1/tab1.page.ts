a20d221ec8e2f67adeeeb684e670bdef453fa0f0
2021030063 <152040336+2021030063@users.noreply.github.com>
2024-07-10 10:36:36 -0500

    ADD: Can logout now

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonButton,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+  ],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(private authService: AuthService) {}
+  logout() {
+    if (confirm('Are you sure you want to log out?')){
+      this.authService.logout();
+    }
+  }
 }
